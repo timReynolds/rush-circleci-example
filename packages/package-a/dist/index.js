9cab6d3c417c050ea5b5c898e8c99322e244c4b4
Tim Reynolds <tim@timothyreynolds.co.uk>
2021-12-30 12:28:37 +0000

    init

---
+++ packages/package-a/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = (name = "World") => {
+    return `Hello ${name}`;
+};
+//# sourceMappingURL=index.js.map
---
+++ packages/package-a/src/index.test.ts
@@ -0,0 +1,12 @@
+import sut from "./index";
+
+describe("Hello", () => {
+  test("Should default to World", () => {
+    expect(sut()).toBe("Hello World");
+  });
+
+  test("When given name, should user name", () => {
+    const name = "Bob";
+    expect(sut(name)).toBe(`Hello ${name}`);
+  });
+});
