42d842ed6f74c6666e24ef777f734ed9684816ee
Tim Reynolds <tim@timothyreynolds.co.uk>
2021-12-30 15:46:34 +0000

    Remove dist files

--- packages/package-a/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (name = "World") => {
-    return `Hello ${name}`;
-};
-//# sourceMappingURL=index.js.map
